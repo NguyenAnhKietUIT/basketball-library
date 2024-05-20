cadc543e68185bc3e7327af14ad28bf56f504ad1
NguyenAnhKietUIT <20521498@gm.uit.edu.vn>
2024-05-20 22:28:48 +0700

    add conversation retrieval test

---
+++ test/conversation-retrieval.py
@@ -0,0 +1,67 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chains import create_history_aware_retriever
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the environment variables
+my_openai_api_key = os.getenv('OPENAI_API_KEY')
+
+# Initialize the OpenAI model instance
+llm = ChatOpenAI(api_key=my_openai_api_key)
+
+# Define the loader
+loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
+
+# Define the documents
+docs = loader.load()
+
+# Define the embedding
+embeddings = OpenAIEmbeddings()
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+# Define the prompt template
+prompt = ChatPromptTemplate.from_messages([
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+    ("user", "Given the above conversation, generate a search query to look up to get information relevant to the conversation")
+])
+
+# Define the retriever and retrieve chain
+retriever = vector.as_retriever()
+retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+chat_history = [HumanMessage(content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
+print(retriever_chain.invoke({
+    "chat_history": chat_history,
+    "input": "Tell me how"
+}))
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+])
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+
+chat_history = [HumanMessage(content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
+print(retrieval_chain.invoke({
+    "chat_history": chat_history,
+    "input": "Tell me how"
+}))
